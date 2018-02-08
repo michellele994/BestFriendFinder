e96612c7b35c7e311ce37a12b2b100ac57ebcb9f
michellele994 <michellele@email.arizona.edu>
2018-02-08 00:30:50 -0700

    Stylized home page, working on survey page

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -7,6 +7,12 @@ module.exports = function(app)
 	app.get("/survey", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
+	app.get("/stylesheet/style.css", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/assets/style.css"));
+	});
+	app.get("/stylesheet/reset.css", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/assets/reset.css"));
+	});
 	app.get("/*", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/home.html"));
 	});
