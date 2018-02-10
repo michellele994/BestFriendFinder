61e1d876f3dfd69cbdce211a59d8b61b1d4618d3
michellele994 <michellele@email.arizona.edu>
2018-02-09 19:56:19 -0700

    added the processenvport

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var parser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
